5be74f45509e68a2298cf4349d9ed70bebe2ba85
Jun-Yoon1 <jun.yn.work@gmail.com>
2025-12-18 09:13:01 +0000

    feat: random num guess

---
+++ main.py
@@ -0,0 +1,34 @@
+import random
+ #1 represents a car
+ #0 represent a goat
+
+stay = 0  #amount won if stay in the same position
+switch = 0 # amount won if you switch 
+
+for i in range(1000):
+    lst = [1,0,0]           # one car and two goats
+    random.shuffle(lst)     # shuffles the list randomly
+    
+    ran = random.randrange(3) # gets a random number for the random guess
+
+    user = lst[ran] #storing the random guess 
+
+    del(lst[ran]) # deleting the random guess
+
+    huh = 0
+
+    for
+        if
+            del
+            break
+
+
+    if
+
+    if
+
+
+
+print
+print
+#Done bu Sam Witton 09/04/2014
